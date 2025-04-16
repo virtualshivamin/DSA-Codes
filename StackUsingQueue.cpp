7c4d6e1ec04b0c764d85a0db2c2c3deb6768f0c2
Shivam Singh <ishivam1202@gmail.com>
2025-04-16 13:25:53 +0530

    Implement Stack using Queue
    Leetcode Problem
    225. Implement Stack using Queues

---
+++ StackUsingQueue.cpp
@@ -0,0 +1,56 @@
+/*
+
+We have to study, stack queue first than other. 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+#define max 5 
+
+    int top=-1;
+    int s[max];
+
+    void push(int a){
+        if (top>=max-1){
+            cout<<"Stack Overflow";
+        }
+        else{
+            s[top+1] = a;
+            top++;
+        }
+    }
+    
+    void pop(){
+        if (top<0){
+            cout<<"Stack Underflow";
+        }
+        else{
+            top--;
+        }
+    }
+    
+    void display()
+    {
+        if (top<0){
+            cout << "Stack is empty."; 
+        }
+        else{
+            cout << "Stack elements : " << endl; 
+            for (int i = 0; i <= top ; i++)
+            {
+                cout << s[i] << endl;
+            }
+        }
+        
+    }
+
+
+int main(){
+
+    
+
+    return 0;
+
+}

50b09ec5d76e3c9f124c83c4479be79da8ec8267
Shivam Singh <ishivam1202@gmail.com>
2025-04-23 08:53:46 +0530

    StackOperations.c

--- StackOperations.c
+++ StackOperations.c
@@ -1,40 +1,100 @@
-#include<stdio.h>
-#include<stdbool.h>
-#define MAX_SIZE 100
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 20
 
-int i,j;
-struct Stack{
-       int items[MAX_SIZE];
-       int top;
-       };
-       
-void createStack(struct Stack *stack){
-     stack->top = -1;
-     }
-     
-bool isFull(struct Stack *stack){
-     return stack-> top == MAX_SIZE -1;
-     }
+int stack[MAX];
+int top = -1;
 
-bool isEmpty(struct Stack *stack){
-     return stack-> top == -1;
-     }
-     
-void push(struct Stack *stack, int value){
-     if(isFull(stack)){
-                       printf("stack overflow can not push element\n", value);
-                       } else {
-                              stack->items[++stack->top] = value;
-                              printf("pushed %d onto the stack.\n", value);
-                              }
-                       }
-                       
-void pop(struct Stack *stack, int value){
-     if(isEmpty(stack)){
-                       printf("stack underflow can not push element\n", value);
-                       } else {
-                              stack->items[++stack->top--];
-                              printf("pushed %d onto the stack.\n", value);
-                              }
-                       }
-                       
+// Function Definitions
+
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow! Cannot push more elements.\n");
+    } else {
+        stack[++top] = value;
+        printf("Value %d pushed to stack.\n", value);
+    }
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow! Nothing to pop.\n");
+    } else {
+        printf("Popped value: %d\n", stack[top--]);
+    }
+}
+
+void peek() {
+    if (top == -1) {
+        printf("Stack is empty.\n");
+    } else {
+        printf("Top of the stack: %d\n", stack[top]);
+    }
+}
+
+void display() {
+    if (top == -1) {
+        printf("Stack is empty.\n");
+    } else {
+        printf("Stack elements (top to bottom):\n");
+        for (int i = top; i >= 0; i--) {
+            printf("%d\n", stack[i]);
+        }
+    }
+}
+
+// Main Function
+int main() {
+    int choice, n, value;
+
+    // Initial input from user
+    printf("How many elements do you want to push initially (max %d)? ", MAX);
+    scanf("%d", &n);
+
+    if (n > MAX) {
+        printf("Cannot insert more than %d elements.\n", MAX);
+        return 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &value);
+        push(value);
+    }
+
+    // Menu-driven operations
+    while (1) {
+        printf("\n--- STACK MENU ---\n");
+        printf("1. PUSH\n");
+        printf("2. POP\n");
+        printf("3. PEEK\n");
+        printf("4. DISPLAY\n");
+        printf("5. EXIT\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to push: ");
+                scanf("%d", &value);
+                push(value);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                peek();
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                printf("Exiting program.\n");
+                exit(0);
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    }
+
+    return 0;
+}

748310c498eaa5d21a7bc472439acab5f0e8c37a
Shivam Singh <ishivam1202@gmail.com>
2025-04-24 14:52:40 +0530

    Two Sum Leetcode

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -36,12 +36,36 @@ Leetcode Problem
     Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 */
 
-#include <iostream> 
-#include <vector> 
-using namespace std;  
+#include <iostream>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    // hello
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    Solution sol;
+    vector<int> result = sol.twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No valid pair found." << endl;
+    }
+
     return 0;
 }

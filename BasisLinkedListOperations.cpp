924377de8eea0143c277c0be600744cceec09a51
Shivam Singh <ishivam1202@gmail.com>
2025-05-25 14:36:32 +0530

    Basis LL operations

--- BasisLinkedListOperations.cpp
+++ BasisLinkedListOperations.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 struct Node{
     int data;
     Node* next;
@@ -35,6 +34,17 @@ int LengthOfLL(Node* head){
     return count;
 }
 
+// here i learned to check if a no is present in LL or not 
+int ifPresentInLL(Node* head, int val){
+    Node* temp = head;
+    while(temp)
+    {
+        if(temp->data == val) return 1; // Returns 1 if present 
+        temp = temp->next;
+    }
+    return 0; // returns 0 if not present 
+}
+
 int main() {
     vector<int> arr = {12,5,8,7};
     Node* head = convertArr2LL(arr);
@@ -47,5 +57,8 @@ int main() {
         temp = temp->next; 
     }
     cout << endl <<  "Length of Linked List : " << LengthOfLL(head) << endl;
+
+    cout << ifPresentInLL(head,5) << endl;
+
     return 0;
 }

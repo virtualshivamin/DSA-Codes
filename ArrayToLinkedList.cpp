fa5888bf5a03a597065ea9e4e25a7c1d58200ec0
Shivam Singh <ishivam1202@gmail.com>
2025-05-25 13:01:23 +0530

    Array to Linked List and Calculating Length of Linked List

--- ArrayToLinkedList.cpp
+++ ArrayToLinkedList.cpp
@@ -23,6 +23,17 @@ Node* convertArr2LL(vector<int> &arr) {
     return head;
 }
 
+int LengthOfLL(Node* head){
+    int count = 0;
+    Node* temp = head;
+    while (temp)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
 int main() {
     vector<int> arr = {12,5,8,7};
     Node* head = convertArr2LL(arr);
@@ -34,5 +45,6 @@ int main() {
         cout << temp->data << " "; 
         temp = temp->next; 
     }
+    cout << endl <<  "Length of Linked List : " << LengthOfLL(head) << endl;
     return 0;
 }

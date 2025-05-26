f0f8c11e5cdc5a5d95dd8a402d3361c61f5cb84d
Shivam Singh <ishivam1202@gmail.com>
2025-05-26 15:16:25 +0530

    Insertion In Linked List (Updated)

--- BasisLinkedListOperations.cpp
+++ BasisLinkedListOperations.cpp
@@ -6,6 +6,11 @@ struct Node{
     Node(int data1)
     {
         data = data1;
+        next = nullptr;
+    }
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
     }
 };
 
@@ -99,6 +104,48 @@ Node* removeK(Node* head, int k){
     return head; // naya head return hoga 
 }
 
+// Head (starting) me insert karne ka function 
+Node* insertHead(Node* head, int val) { // Yaha head aur ek value paas hoga jo insert karana hai 
+    Node* temp = new Node(val,head); // pehle value chalega, fir head chal jayega (aise insert ho jayega) aur temp me store hoga 
+    return temp; // Temp return ho jayega 
+}
+
+// Tail (ending) me insert karne ka function 
+Node* insertTail(Node* head, int val) { // Yaha head aur ek value paas hoga jo insert karana hai 
+    
+    if(head == NULL) return new Node(val); // agar LL khali hai to wahi element ek return ho jayega 
+
+    Node* temp = head; // temp variable jisme head store hai 
+    while(temp->next != NULL) temp = temp->next; // Loop ko aakhri point pe roko 
+    Node* newNode = new Node(val); // waha ek newNode banao jisme val store ho 
+    temp->next = newNode; // naya node ko temp ke aage jod do 
+    return head; // naya head (LL) return kar do 
+}
+
+Node* insertAtPosition(Node* head, int val, int pos) {
+    if(head == NULL) { // agar head khali hai to insert kar do 
+        if(pos==1) // agar pos 1 hai tabhi insert karo 
+            return new Node(val); 
+        else // warna jo hai wo return kar do 
+            return head;
+    }
+
+    if(pos == 1) return new Node(val, head); // agar LL khali nahi hai aur 1st position pe insert karna hai to 
+
+    int count = 0;
+    Node* temp = head; // temp variable jisme head store hai 
+    while(temp!=NULL) { // Loop tab tak chalega jab tak last value pe naa pahuch jaye 
+        count++; 
+        if(count==(pos-1)) { // last se ek pehle tak ya beech me jab count match kar jaye 
+            Node* x = new Node(val, temp->next); // uske ek aage insert karenge 
+            temp->next = x; // ek aage me value daal denge 
+            break; 
+        }
+        temp = temp->next; // loop aage badhane ke liye 
+    }
+    return head; // naya LL return kar denge 
+}
+
 // Here i learned Traversal in Linked List by print function 
 void print(Node* head) 
 {
@@ -131,6 +178,18 @@ int main() {
 
     // beech me ya last me ka koi bhi k th element delete (jaise yaha 3 rd element delete ho raha hai) 
     head = removeK(head,3);
+    print(head); 
+
+    // Starting point me value store karna (head) me 
+    head = insertHead(head,100); 
+    print(head); 
+
+    // End point me value store karna (tail) me 
+    head = insertTail(head,10); 
+    print(head); 
+
+    // Beech me value store karna me 
+    head = insertAtPosition(head,1000,4); 
     print(head); 
 
     return 0;
